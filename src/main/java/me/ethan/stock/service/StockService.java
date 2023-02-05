db18997485020c1a29bc83b70005e214885ac31a
EthanKim <hey0507@gmail.com>
2023-02-05 23:09:15 +0900

    Update : 동시성 문제발견 및 해결 - synchronized 의 문제점 - DataBase 를 통해서 동시성 문제를 해결 하는 방법 (Lock)

--- src/main/java/me/ethan/stock/service/StockService.java
+++ src/main/java/me/ethan/stock/service/StockService.java
@@ -4,21 +4,23 @@ import me.ethan.stock.domain.Stock;
 import me.ethan.stock.repository.StockRepository;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+
 @Service
 public class StockService {
 
-    private StockRepository stockRepository;
+    private final StockRepository stockRepository;
 
     public StockService(StockRepository stockRepository) {
         this.stockRepository = stockRepository;
     }
 
-    public void decreaseStock(Long id, Long quantity){
+
+//    @Transactional
+    public synchronized void decreaseStock(Long id, Long quantity){
         //get stock
         //재고 감소
         //저장
-
-
         Stock stock = stockRepository.findById(id).orElseThrow();
         stock.decreaseStock(quantity);
         stockRepository.saveAndFlush(stock);
